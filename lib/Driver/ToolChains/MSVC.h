ee1e5b9d368525de4bcdebc8cb470bdc4e70aec4
Albert Ziegenhagel <albert.ziegenhagel@scai.fraunhofer.de>
2017-10-26 17:09:30 +0200

    Add support to set linker flags for flang on windows

--- lib/Driver/ToolChains/MSVC.h
+++ lib/Driver/ToolChains/MSVC.h
@@ -106,6 +106,9 @@ public:
       const llvm::opt::ArgList &DriverArgs,
       llvm::opt::ArgStringList &CC1Args) const override;
 
+  void AddFortranStdlibLibArgs(const llvm::opt::ArgList &DriverArgs,
+                               llvm::opt::ArgStringList &CC1Args) const override;
+
   void AddCudaIncludeArgs(const llvm::opt::ArgList &DriverArgs,
                           llvm::opt::ArgStringList &CC1Args) const override;
 
